d69b92ee948e55163bc939fba98f1b01b606e247
monikarangari <117535755+monikarangari@users.noreply.github.com>
2024-12-20 11:45:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,24 +2,8 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-}
-
-variable "file_path" {
-  description = "The path to the file to upload"
-  type        = string
-}
-
-variable "allowed_ip" {
-  description = "The IP address allowed to access the S3 bucket"
-  type        = string
-}
-
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
-  acl    = "private"
 }
 
 resource "aws_s3_bucket_policy" "my_bucket_policy" {
