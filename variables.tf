da1d87ff09fee431836c5d6b169b3026825c0d95
monikarangari <117535755+monikarangari@users.noreply.github.com>
2024-12-20 10:56:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "bucket_name" {
   description = "The name of the S3 bucket"
-  type        = "monika-voda-assignment"
+  type        = string
 }
 
 variable "file_path" {
   description = "The path to the file to upload"
-  type        = "/d/index.html"
+  type        = string
 }
 
 variable "allowed_ip" {
   description = "The IP address allowed to access the S3 bucket"
-  type        = "103.189.98.196"
+  type        = string
 }
